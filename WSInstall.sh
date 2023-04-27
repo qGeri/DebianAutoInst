b94d88eb5bfd07a36be3de9ab0a688d9d135d5ce
qGeri <126606757+qGeri@users.noreply.github.com>
2023-04-27 20:36:35 +0200

    Update WSInstall.sh

--- WSInstall.sh
+++ WSInstall.sh
@@ -8,7 +8,7 @@ echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 echo 'Swapfile created!'
 echo 'Add repositories!'
 apt install -y wget
-echo 'deb http://deb.debian.org/debian bullseye-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list
+echo 'deb http://deb.debian.org/debian bookworm-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list
 #mono
 apt install -y apt-transport-https dirmngr gnupg ca-certificates
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
@@ -29,7 +29,9 @@ wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
 echo 'Install packages!'
 apt update -y
 apt upgrade -y
-apt install -y gnome-session firefox-esr gnome-calculator gnome-calendar gnome-clocks gnome-disk-utility gnome-tweaks synaptic gedit nautilus eog gnome-shell-extensions gnome-shell-extension-appindicator gnome-shell-extension-bluetooth-quick-connect gnome-shell-extension-dashtodock gnome-system-monitor gnome-terminal thunderbird g3dviewer gimp gimp-data-extras inkscape krita blender mono-complete dotnet-sdk-7.0 codium unityhub vlc libreoffice gufw clamtk clamtk-gnome dconf-editor
+apt install -y gnome-session firefox-esr gnome-calculator gnome-calendar gnome-clocks gnome-disk-utility gnome-tweaks synaptic gedit nautilus eog 
+apt install -y gnome-shell-extensions gnome-shell-extension-appindicator gnome-shell-extension-bluetooth-quick-connect gnome-shell-extension-dashtodock gnome-system-monitor gnome-terminal thunderbird
+apt install -y g3dviewer gimp gimp-data-extras inkscape krita blender mono-complete dotnet-sdk-7.0 codium unityhub vlc libreoffice gufw clamtk clamtk-gnome dconf-editor
 apt update -y
 apt upgrade -y
 echo 'Configure!'
