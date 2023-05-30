ddb3e08f856b7285975d55b0b5a3ee39a2a6cc45
qGeri <126606757+qGeri@users.noreply.github.com>
2023-05-30 16:52:25 +0200

    Update WSInstall.sh

--- WSInstall.sh
+++ WSInstall.sh
@@ -17,12 +17,12 @@ echo "deb https://download.mono-project.com/repo/debian stable-buster main" | su
 wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
-#vsCodium
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-    | gpg --dearmor \
-    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
-echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
-    | sudo tee /etc/apt/sources.list.d/vscodium.list
+#vsCode
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
 #Unity
 sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
@@ -33,9 +33,13 @@ echo 'Install packages!'
 apt update -y
 apt upgrade -y
 apt install -y gnome-session gnome-calculator gnome-package-updater lollypop gnome-sound-recorder gnome-system-monitor gnome-terminal planner file-roller gnome-calendar gnome-clocks gnome-disk-utility gnome-tweaks synaptic gedit gdebi nautilus nautilus-admin nautilus-data eog gcolor3 gedit-plugins gnome-passwordsafe gnome-font-viewer
-apt install -y gnome-shell-extensions lm-sensors gnome-shell-extension-dash-to-panel gnome-shell-extension-arc-menu gnome-shell-extension-top-icons-plus gnome-shell-extension-bluetooth-quick-connect gnome-shell-extension-desktop-icons-ng gnome-shell-extension-tiling-assistant 
-apt install -y firefox-esr thunderbird g3dviewer gimp gimp-data-extras gimp-gmic inkscape krita krita-gmic blender audacity lmms lmms-common lmms-vst-server mono-complete dotnet-sdk-7.0 codium unityhub vlc libreoffice gufw clamtk clamtk-gnome dconf-editor spotify-client
+apt install -y firefox-esr thunderbird g3dviewer vlc libreoffice gufw clamtk clamtk-gnome dconf-editor
 apt install -y mesa-utils mesa-utils-extra mesa-vulkan-drivers mesa-vdpau-drivers mesa-va-drivers vulkan-tools vulkan-validationlayers libvulkan1 vkd3d-compiler vkbasalt primus-vk radeontop libavcodec-extra 
+#apt install -y gnome-shell-extensions lm-sensors gnome-shell-extension-dash-to-panel gnome-shell-extension-arc-menu gnome-shell-extension-top-icons-plus gnome-shell-extension-bluetooth-quick-connect gnome-shell-extension-desktop-icons-ng gnome-shell-extension-tiling-assistant
+#apt install -y gimp gimp-data-extras gimp-gmic inkscape krita krita-gmic blender audacity lmms lmms-common lmms-vst-server mono-complete dotnet-sdk-7.0 unityhub spotify-client
+#apt install apt-transport-https
+#apt update
+#apt install code
 apt update -y
 apt upgrade -y
 echo 'Configure!'
