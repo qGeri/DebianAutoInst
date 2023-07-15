6d350198cf0fefbfa58f21c85ea4b4b4bf7662a8
qGeri <126606757+qGeri@users.noreply.github.com>
2023-07-15 12:25:07 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -12,6 +12,6 @@ apt upgrade -y
 apt install -y wget
 ##################################################################################################################################################
 echo 'Install Desktop Environment!'
-apt install xfce4-session
-apt install dbus-user-session light-locker 
-apt install thunar thunar-archive-plugin thunar-font-manager thunar-media-tags-plugin thunar-volman xfce4-goodies xfce4-terminal
+apt install -y xfce4-session
+apt install -y dbus-user-session light-locker 
+apt install -y thunar thunar-archive-plugin thunar-font-manager thunar-media-tags-plugin thunar-volman xfce4-goodies xfce4-terminal
