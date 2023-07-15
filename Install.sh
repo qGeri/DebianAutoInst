c9690f6dabd57d046994f3fda51f9dc868af499e
qGeri <126606757+qGeri@users.noreply.github.com>
2023-07-15 10:54:08 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,5 +6,7 @@ echo 'deb http://deb.debian.org/debian bookworm main contrib non-free non-free-f
 echo 'deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware' | tee -a /etc/apt/sources.list
 echo 'deb http://deb.debian.org/debian bookworm-backports main contrib non-free non-free-firmware' | tee -a /etc/apt/sources.list
 echo 'deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware' | tee -a /etc/apt/sources.list
-
+apt update -y
+apt upgrade -y
+##################################################################################################################################################
 apt install -y wget
