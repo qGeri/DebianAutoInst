672352d8f68a9081477ae9d15980b1ee2137c62f
qGeri <126606757+qGeri@users.noreply.github.com>
2023-07-19 21:05:46 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -31,6 +31,8 @@ rm -f packages.microsoft.gpg
 echo 'Add Unity Engine repositories!'
 sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
-##################################################################################################################################################
+######################################################################################################################################################
+apt update -y
+apt upgrade -y
 echo 'Install Desktop Environment!'
 
