5e1953f210e7ac3a64ae2e30dee17c0ab8590b16
qGeri <126606757+qGeri@users.noreply.github.com>
2023-07-20 17:57:30 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -36,7 +36,7 @@ apt update -y
 apt upgrade -y
 echo 'Install Desktop Environment!'
 apt install -y gnome-session gnome-calculator gnome-package-updater gnome-sound-recorder gnome-system-monitor gnome-terminal planner file-roller gnome-calendar gnome-clocks gnome-disk-utility gparted gnome-tweaks synaptic gedit gdebi nautilus nautilus-admin nautilus-data nautilus-extension-gnome-terminal nautilus-image-converter eog eog-plugins gcolor3 gedit-plugins gnome-passwordsafe gnome-shell-extension-manager gnome-font-viewer
-apt install -y firefox-esr thunderbird g3dviewer vlc libreoffice gufw clamtk clamtk-gnome dconf-editor jstest-gtk
+apt install -y firefox-esr firefox-esr-l10n-hu thunderbird g3dviewer vlc libreoffice gufw clamtk clamtk-gnome dconf-editor jstest-gtk
 apt install -y mesa-utils mesa-utils-extra mesa-vulkan-drivers mesa-vdpau-drivers mesa-va-drivers vulkan-tools vulkan-validationlayers libvulkan1 vkd3d-compiler vkbasalt primus-vk radeontop libavcodec-extra lm-sensors 
 apt install -y gimp gimp-data-extras gimp-gmic inkscape blender audacity mono-complete dotnet-sdk-7.0 unityhub
 apt install -y apt-transport-https
