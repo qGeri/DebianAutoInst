84a56d117573674364813a1339921b83a906de00
qGeri <126606757+qGeri@users.noreply.github.com>
2023-10-13 23:56:47 +0200

    Update Win.sh

--- Win.sh
+++ Win.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ##################################################################################################################################################
 echo 'Add repositories!'
+apt install -y wget
 sed -i '1,$d' /etc/apt/sources.list
 echo 'deb http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware' | tee -a /etc/apt/sources.list
 echo 'deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware' | tee -a /etc/apt/sources.list
